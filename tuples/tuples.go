1d07bbabd982c677c0c60a06d528b78544bcab78
Kris Holdich <kris@smkd.net>
2019-07-10 09:56:35 +0100

    Vector and Point helpers

--- tuples/tuples.go
+++ tuples/tuples.go
@@ -4,12 +4,46 @@ type tuple struct {
 	x, y, z, w	float32
 }
 
-func (t *tuple) Init(x, y, z, w float32) *tuple {
+func (t *tuple) Tuple(x, y, z, w float32) *tuple {
 	t.x = x
 	t.y = y
 	t.z = z
 	t.w = w
 	return t
+}
+
+func (t *tuple) Point(x, y, z float32) *tuple {
+	t.x = x
+	t.y = y
+	t.z = z
+	t.w = 1.0
+	return t
+}
+
+func (t *tuple) Vector(x, y, z float32) *tuple {
+	t.x = x
+	t.y = y
+	t.z = z
+	t.w = 0.0
+	return t
+}
+
+func Tuple(x, y, z, w float32) *tuple {
+	t := tuple{}
+	t.Tuple(x, y, z, w)
+	return &t
+}
+
+func Point(x, y, z float32) *tuple {
+	t := tuple{}
+	t.Point(x, y, z)
+	return &t
+}
+
+func Vector(x, y, z float32) *tuple {
+	t := tuple{}
+	t.Vector(x, y, z)
+	return &t
 }
 
 func (t *tuple) IsAVector() bool {
